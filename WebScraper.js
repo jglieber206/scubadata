3b6c4358d811c56dfb4987ae3011dfe643fa9645
annasun17 <anna.sun1017@gmail.com>
2016-11-08 22:47:11 -0700

    Update WebScraper.js

--- WebScraper.js
+++ WebScraper.js
@@ -7,48 +7,65 @@
 
 const request = require("tinyreq");
 
-//int urlList
-//for (urlList = 0; urlList < 10; urlList ++) {
-//    request( urlList[i] , function (err, body) {
-//        console.log(err || body); // Print out the HTML
-//    }); 
-//}
+var urlArray = new Array();
 
-request("http://ionicabizau.net/", function (err, body) {
+for (int i = 0; i < 10; i ++) {
+    request(urlArray[i], function (err, body) {
     console.log(err || body); // Print out the HTML
-});
+    });
+}
 
-const cheerio = require("cheerio");
+const cheerio = require("cheerio"); //WTF is a cheerio?
 
 // Parse the HTML 
-let $ = cheerio.load("<h2 class="title">Hello world</h2>");
-
+        let $ = cheerio.load("<h2 class="title">Hello world</h2>");
 // Take the h2.title element and show the text
-console.log($("h2.title").text());
+        console.log($("h2.title").text());
 // => Hello world
 
 // Import the dependencies
-const cheerio = require("cheerio")
-    , req = require("tinyreq")
-    ;
-
+        const cheerio = require("cheerio")
+        , req = require("tinyreq")
+        ;
 // Define the scrape function
-function scrape(url, data, cb) {
-    // 1. Create the request
-    req(url, (err, body) => {
+        function scrape(url, data, cb) {
+        // 1. Create the request
+        req(url, (err, body) = > {
         if (err) { return cb(err); }
 
         // 2. Parse the HTML
         let $ = cheerio.load(body)
-          , pageData = {}
-          ;
-
-        // 3. Extract the data
-        Object.keys(data).forEach(k => {
-            pageData[k] = $(data[k]).text();
+                , pageData = {}
+        ;
+                // 3. Extract the data
+                Object.keys(data).forEach(k = > {
+        pageData[k] = $(data[k]).text();
         });
+                // Send the data in the callback
+                cb(null, pageData);
+        });
+                }
 
-        // Send the data in the callback
-        cb(null, pageData);
-    });
+// Get articles in a list
+for (int i = 0; i < 10; i ++) {
+scrapeIt(urlArray[i], {
+listItem: ".article"
+        , name: "articles"
+        , data: {
+        createdAt: {
+        selector: ".date"
+                , convert: x = > new Date(x)
+        }
+        , title: "a.article-title"
+                , tags: {
+                selector: ".tags"
+                        , convert: x = > x.split("|").map(c = > c.trim()).slice(1)
+                }
+        , content: {
+        selector: ".article-content"
+                , how: "html"
+        }
+        }
+}, (err, page) = > {
+console.log(err || page);
 }
